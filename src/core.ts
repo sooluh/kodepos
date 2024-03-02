4e665f06805812eb5b7923d712d5ee8a8e968431
Abu Masyail <suluh.webdevelopers@hotmail.com>
2024-03-02 15:02:27 +0700

    perf: remove carikodepos.com until ssl is fixed

--- src/core.ts
+++ src/core.ts
@@ -6,7 +6,6 @@ import type { FastifyInstance, FastifyPluginOptions } from 'fastify'
 const load = async (app: FastifyInstance, _: FastifyPluginOptions) => {
   const providers = new SequentialRoundRobin<ProviderList>([
     { hostname: 'direktorikodepos.org', segment: 'wilayah' },
-    { hostname: 'carikodepos.com', segment: 'daerah' },
     { hostname: 'nomorkodepos.com', segment: 'di' },
   ])
 
